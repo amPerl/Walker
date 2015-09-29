7228d6417004dbbb7db036d4dfd21d515abcb49e
Chris Latham <BackwardSpy@gmail.com>
2015-09-29 02:36:01 +0100

    Added TMX compiler project.

---
+++ tmxc/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TiledSharp;
+
+namespace tmxc
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var map = new TmxMap(args[0]);
+
+      Console.WriteLine($"Loaded map {args[0]}");
+      Console.WriteLine($"Version: {map.Version}");
+      Console.WriteLine($"Background colour: {map.BackgroundColor.R}, {map.BackgroundColor.G}, {map.BackgroundColor.B}");
+      Console.WriteLine($"Size: {map.Width}, {map.Height}");
+      Console.WriteLine($"Tile size: {map.TileWidth}, {map.TileHeight}");
+      Console.WriteLine();
+
+      foreach(var tileset in map.Tilesets)
+      {
+        Console.WriteLine($"Tileset: {tileset.Name}");
+        Console.WriteLine($"\tPath: {tileset.Image.Source}");
+        Console.WriteLine();
+      }
+
+      foreach(var layer in map.Layers)
+      {
+        Console.WriteLine($"Layer: {layer.Name}");
+        Console.WriteLine();
+      }
+    }
+  }
+}
