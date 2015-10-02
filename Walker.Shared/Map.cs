c46e9a4eb31bd5dc02ddc0e177b0acca03d1b56d
Chris Latham <BackwardSpy@gmail.com>
2015-10-03 00:06:55 +0100

    Minor style changes.

--- Walker.Shared/Map.cs
+++ Walker.Shared/Map.cs
@@ -6,142 +6,145 @@ using System.Text;
 
 namespace Walker.Shared.Map
 {
-    static class BinaryExtensions
+  static class BinaryExtensions
+  {
+    public static void WriteFixedString(this BinaryWriter writer, string str)
     {
-        public static void WriteFixedString(this BinaryWriter writer, string str)
-        {
-            byte[] strBuffer = Encoding.UTF8.GetBytes(str);
-            writer.Write((ushort)strBuffer.Length);
-            writer.Write(strBuffer);
-        }
-
-        public static string ReadFixedString(this BinaryReader reader)
-        {
-            ushort bufferLength = reader.ReadUInt16();
-            byte[] strBuffer = reader.ReadBytes(bufferLength);
-            return Encoding.UTF8.GetString(strBuffer);
-        }
+      var strBuffer = Encoding.UTF8.GetBytes(str);
+      writer.Write((ushort)strBuffer.Length);
+      writer.Write(strBuffer);
     }
 
-    public class Map
+    public static string ReadFixedString(this BinaryReader reader)
     {
-        public List<Tileset> Tilesets;
+      var bufferLength = reader.ReadUInt16();
+      var strBuffer = reader.ReadBytes(bufferLength);
+      return Encoding.UTF8.GetString(strBuffer);
+    }
+  }
 
-        public Map()
-        {
-            Tilesets = new List<Tileset>();
-        }
+  public class Map
+  {
+    public List<Tileset> Tilesets;
 
-        public static Map Read(BinaryReader reader)
-        {
-            Map map = new Map();
-
-            int tilesetCount = reader.ReadInt32();
-            for (int i = 0; i < tilesetCount; i++)
-                map.Tilesets.Add(Tileset.Read(reader));
-
-            return map;
-        }
-
-        public void Write(BinaryWriter writer)
-        {
-            writer.Write(Tilesets.Count);
-
-            foreach (var tileset in Tilesets)
-                tileset.Write(writer);
-        }
+    public Map()
+    {
+      Tilesets = new List<Tileset>();
     }
 
-    public class Tileset
+    public static Map Read(BinaryReader reader)
     {
-        public string Name, ImagePath;
-        public int Margin, Spacing, TileCount, TileWidth, TileHeight, FirstGid;
-        public List<TilePropertyEntry> PropertyEntries;
+      var map = new Map();
 
-        public Tileset(string name, string imagePath)
-        {
-            Name = name;
-            ImagePath = imagePath;
-            PropertyEntries = new List<TilePropertyEntry>();
-        }
+      int tilesetCount = reader.ReadInt32();
 
-        public static Tileset Read(BinaryReader reader)
-        {
-            string name = reader.ReadFixedString();
-            string imagePath = reader.ReadFixedString();
+      for (int i = 0; i < tilesetCount; i++)
+        map.Tilesets.Add(Tileset.Read(reader));
 
-            Tileset tileset = new Tileset(name, imagePath)
-            {
-                Margin = reader.ReadInt32(),
-                Spacing = reader.ReadInt32(),
-                TileCount = reader.ReadInt32(),
-                TileWidth = reader.ReadInt32(),
-                TileHeight = reader.ReadInt32(),
-                FirstGid = reader.ReadInt32()
-            };
-
-            int propertyCount = reader.ReadInt32();
-            for (int i = 0; i < propertyCount; i++)
-                tileset.PropertyEntries.Add(TilePropertyEntry.Read(reader));
-
-            return tileset;
-        }
-
-        public void Write(BinaryWriter writer)
-        {
-            writer.WriteFixedString(Name.ToLower());
-            writer.WriteFixedString(ImagePath.ToLower());
-            writer.Write(Margin);
-            writer.Write(Spacing);
-            writer.Write(TileCount); // TODO: write actual tiles?
-            writer.Write(TileWidth);
-            writer.Write(TileHeight);
-            writer.Write(FirstGid);
-            
-            writer.Write(PropertyEntries.Count);
-            foreach (var entry in PropertyEntries)
-                entry.Write(writer);
-        }
+      return map;
     }
 
-    public class TilePropertyEntry
+    public void Write(BinaryWriter writer)
     {
-        public int Id;
-        public Dictionary<string, string> Properties;
+      writer.Write(Tilesets.Count);
 
-        public TilePropertyEntry()
-        {
-            Properties = new Dictionary<string, string>();
-        }
-
-        public static TilePropertyEntry Read(BinaryReader reader)
-        {
-            TilePropertyEntry tile = new TilePropertyEntry
-            {
-                Id = reader.ReadInt32()
-            };
-
-            int propertyCount = reader.ReadInt32();
-            for (int i = 0; i < propertyCount; i++)
-            {
-                string key = reader.ReadFixedString();
-                string value = reader.ReadFixedString();
-                tile.Properties.Add(key, value);
-            }
-
-            return tile;
-        }
-
-        public void Write(BinaryWriter writer)
-        {
-            writer.Write(Id);
-            writer.Write(Properties.Count);
-
-            foreach (var property in Properties)
-            {
-                writer.WriteFixedString(property.Key.ToLower());
-                writer.WriteFixedString(property.Value.ToLower());
-            }
-        }
+      foreach (var tileset in Tilesets)
+        tileset.Write(writer);
     }
+  }
+
+  public class Tileset
+  {
+    public string Name, ImagePath;
+    public int Margin, Spacing, TileCount, TileWidth, TileHeight, FirstGid;
+    public List<TilePropertyEntry> PropertyEntries;
+
+    public Tileset(string name, string imagePath)
+    {
+      Name = name;
+      ImagePath = imagePath;
+      PropertyEntries = new List<TilePropertyEntry>();
+    }
+
+    public static Tileset Read(BinaryReader reader)
+    {
+      var name = reader.ReadFixedString();
+      var imagePath = reader.ReadFixedString();
+
+      var tileset = new Tileset(name, imagePath)
+      {
+        Margin = reader.ReadInt32(),
+        Spacing = reader.ReadInt32(),
+        TileCount = reader.ReadInt32(),
+        TileWidth = reader.ReadInt32(),
+        TileHeight = reader.ReadInt32(),
+        FirstGid = reader.ReadInt32()
+      };
+
+      var propertyCount = reader.ReadInt32();
+
+      for (var i = 0; i < propertyCount; i++)
+        tileset.PropertyEntries.Add(TilePropertyEntry.Read(reader));
+
+      return tileset;
+    }
+
+    public void Write(BinaryWriter writer)
+    {
+      writer.WriteFixedString(Name.ToLower());
+      writer.WriteFixedString(ImagePath.ToLower());
+      writer.Write(Margin);
+      writer.Write(Spacing);
+      writer.Write(TileCount);
+      writer.Write(TileWidth);
+      writer.Write(TileHeight);
+      writer.Write(FirstGid);
+
+      writer.Write(PropertyEntries.Count);
+
+      foreach (var entry in PropertyEntries)
+        entry.Write(writer);
+    }
+  }
+
+  public class TilePropertyEntry
+  {
+    public int Id;
+    public Dictionary<string, string> Properties;
+
+    public TilePropertyEntry()
+    {
+      Properties = new Dictionary<string, string>();
+    }
+
+    public static TilePropertyEntry Read(BinaryReader reader)
+    {
+      var tile = new TilePropertyEntry
+      {
+        Id = reader.ReadInt32()
+      };
+
+      var propertyCount = reader.ReadInt32();
+      for (var i = 0; i < propertyCount; i++)
+      {
+        var key = reader.ReadFixedString();
+        var value = reader.ReadFixedString();
+        tile.Properties.Add(key, value);
+      }
+
+      return tile;
+    }
+
+    public void Write(BinaryWriter writer)
+    {
+      writer.Write(Id);
+      writer.Write(Properties.Count);
+
+      foreach (var property in Properties)
+      {
+        writer.WriteFixedString(property.Key.ToLower());
+        writer.WriteFixedString(property.Value.ToLower());
+      }
+    }
+  }
 }
--- tmxc/Program.cs
+++ tmxc/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using TiledSharp;
@@ -7,60 +6,62 @@ using Walker.Shared.Map;
 
 namespace tmxc
 {
-    class Program
+  class Program
+  {
+    static TmxMap _map;
+
+    static void Main(string[] args)
     {
-        static TmxMap _map;
+      if (args.Length < 2)
+      {
+        Console.WriteLine("Usage: tmxc source dest");
+        return;
+      }
 
-        static void Main(string[] args)
+      try
+      {
+        _map = new TmxMap(args[0]);
+      }
+      catch
+      {
+        Console.WriteLine($"Failed to open map file '{args[0]}'");
+        return;
+      }
+
+      var walkerMap = new Map();
+
+      foreach (var tileset in _map.Tilesets)
+      {
+        var walkerTileset = new Tileset(tileset.Name, tileset.Image.Source)
         {
-            if (args.Length < 2)
-            {
-                Console.WriteLine("Usage: tmxc source dest");
-                return;
-            }
+          Margin = tileset.Margin,
+          Spacing = tileset.Spacing,
+          TileCount = (int)tileset.TileCount,
+          TileWidth = tileset.TileWidth,
+          TileHeight = tileset.TileHeight,
+          FirstGid = tileset.FirstGid
+        };
 
-            try
-            {
-                _map = new TmxMap(args[0]);
-            }
-            catch
-            {
-                Console.WriteLine($"Failed to open map file '{args[0]}'");
-                return;
-            }
+        foreach (var tile in tileset.Tiles.Where(tile => tile.Properties.Count > 0))
+        {
+          var entry = new TilePropertyEntry { Id = tile.Id };
 
-            Map walkerMap = new Map();
-            foreach (var tileset in _map.Tilesets)
-            {
-                Tileset walkerTileset = new Tileset(tileset.Name, tileset.Image.Source)
-                {
-                    Margin = tileset.Margin,
-                    Spacing = tileset.Spacing,
-                    TileCount = (int)tileset.TileCount,
-                    TileWidth = tileset.TileWidth,
-                    TileHeight = tileset.TileHeight,
-                    FirstGid = tileset.FirstGid
-                };
+          foreach (var kvp in tile.Properties)
+            entry.Properties.Add(kvp.Key, kvp.Value);
 
-                foreach (var tile in tileset.Tiles.Where(tile => tile.Properties.Count > 0))
-                {
-                    TilePropertyEntry entry = new TilePropertyEntry { Id = tile.Id };
-
-                    foreach (var kvp in tile.Properties)
-                        entry.Properties.Add(kvp.Key, kvp.Value);
-
-                    walkerTileset.PropertyEntries.Add(entry);
-                }
-
-                walkerMap.Tilesets.Add(walkerTileset);
-            }
-
-            var dest = args[1] + ".map";
-            using (var fs = new FileStream(dest, FileMode.Create))
-            using (var writer = new BinaryWriter(fs))
-            {
-                walkerMap.Write(writer);
-            }
+          walkerTileset.PropertyEntries.Add(entry);
         }
+
+        walkerMap.Tilesets.Add(walkerTileset);
+      }
+
+      var dest = args[1] + ".map";
+
+      using (var fs = new FileStream(dest, FileMode.Create))
+      using (var writer = new BinaryWriter(fs))
+      {
+        walkerMap.Write(writer);
+      }
     }
+  }
 }
